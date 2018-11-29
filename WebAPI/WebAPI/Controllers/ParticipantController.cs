b62dc42a479a2f078f27f26f99042ec5c91c7670
Kalyan Alapati <Kalyan_Alapati@epam.com>
2018-11-29 20:04:26 +0530

    till registration

---
+++ WebAPI/WebAPI/Controllers/ParticipantController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebAPI.Controllers
+{
+    [EnableCors(origins:"*", headers:"*", methods:"*")]
+    public class ParticipantController : ApiController
+    {
+        [HttpPost]
+        [Route("api/InsertParticipant")]
+        public Participant Insert(Participant participant)
+        {
+            using (DBModel model = new DBModel())
+            {
+                model.Participants.Add(participant);
+                model.SaveChanges();
+            }
+            return participant;
+        }
+
+        [HttpPut]
+        [Route("api/UpdateParticipant")]
+        public void UpdateParticipant(Participant participant)
+        {
+            using (DBModel model = new DBModel())
+            {
+                model.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                model.SaveChanges();
+            }
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Controllers/QuizController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class QuizController : ApiController
+    {
+        [HttpGet]
+        [Route("api/GetQuestions")]
+        public HttpResponseMessage Get()
+        {
+            using (DBModel model = new DBModel())
+            {
+                var questions = model.Questions
+                        .Select(q => new { QnID = q.QnID, Qn = q.Qn, Image = q.Image, q.Option1, q.Option2, q.Option3, q.Option4 })
+                        .OrderBy(y => Guid.NewGuid())
+                        .Take(10)
+                        .ToArray();
+
+                var updated = questions.AsEnumerable()
+                        .Select(x => 
+                        new {
+                            QnID = x.QnID,
+                            Qn = x.Qn,
+                            Image = x.Image,
+                            Options = new string[] {x.Option1, x.Option2, x.Option3, x.Option4}
+                        }).ToList();
+                return this.Request.CreateResponse(HttpStatusCode.OK, updated);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/GetAnswers")]
+        public HttpResponseMessage GetAnswers(int[] QnIds)
+        {
+            using (DBModel model = new DBModel())
+            {
+                var qnAnswers = model.Questions.AsEnumerable()
+                        .Where(q => QnIds.Contains(q.QnID))
+                        .OrderBy(x => { return Array.IndexOf(QnIds, x.QnID); })
+                        .Select(z => z.Answer)
+                        .ToArray();
+                return Request.CreateResponse(HttpStatusCode.OK, qnAnswers);
+            }
+        }
+
+
+    }
+}
